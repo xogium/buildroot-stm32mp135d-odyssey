c7f9e6097a871b204473d96aedaadf8376c8810b
Xogium <16747321+xogium@users.noreply.github.com>
2023-10-16 07:09:17 +0200

    configs/stm32mp135d_odyssey: added boot script
    Lets use boot.scr to boot the board properly on eMMC or micro sd.
    
    * configs/stm32mp135d_odyssey_defconfig enables host-uboot-tools package
    and gives boot.cmd as script source
    * boot.cmd in board/stm32mp135d-odyssey/utilities contains the text form
    of the script
    * rootfs_postbuil.sh in board/stm32mp135d-odyssey/scripts copies the
    boot.scr file into the target

--- board/stm32mp135d-odyssey/scripts/rootfs_postbuild.sh
+++ board/stm32mp135d-odyssey/scripts/rootfs_postbuild.sh
@@ -8,3 +8,7 @@ dd if="${BINARIES_DIR}/tf-a-stm32mp135d-odyssey.stm32" \
 dd if="${BINARIES_DIR}/fip.bin" \
 	of="${BINARIES_DIR}/combined-tf-a-and-fip.img" \
 	oflag=seek_bytes seek=256K
+
+# copy boot.scr into the target
+cp "${BINARIES_DIR}/boot.scr" \
+	"${TARGET_DIR}/boot/boot.scr"
---
+++ board/stm32mp135d-odyssey/utilities/boot.cmd
@@ -0,0 +1,15 @@
+if test $boot_device = "mmc" && test $boot_instance = "0"; then
+	echo "Micro sd card boot" &&
+	ext4load mmc 0:5 ${fdt_addr_r} /boot/stm32mp135d-odyssey.dtb &&
+	ext4load mmc 0:5 ${kernel_addr_r} /boot/zImage &&
+	setenv bootargs "root=/dev/mmcblk0p5 rootwait rw quiet console=ttySTM0,115200n8 earlycon";
+fi
+
+if test $boot_device = "mmc" && test $boot_instance = "1"; then
+	echo "eMMC boot" &&
+	ext4load mmc 1:2 ${fdt_addr_r} /boot/stm32mp135d-odyssey.dtb &&
+	ext4load mmc 1:2 ${kernel_addr_r} /boot/zImage &&
+	setenv bootargs "root=/dev/mmcblk1p2 rootwait rw quiet console=ttySTM0,115200n8 earlycon";
+fi
+
+bootz ${kernel_addr_r} - ${fdt_addr_r}
